38284abcc56a26bceaf1a7050cc785ac293ebbed
Vincent <v3rdugo@yahoo.com>
2023-05-21 22:31:05 -0700

    create deployment script

---
+++ scripts/1_deploy.js
@@ -0,0 +1,19 @@
+async function main()
+{
+    // Fetch contract to deploy
+    const Token = await ethers.getContractFactory("Token")
+
+    // Deploy contract
+    const token = await Token.deploy()
+
+    await token.deployed()
+
+    console.log(`Token deployed to address: ${token.address}`)
+}
+  
+  // We recommend this pattern to be able to use async/await everywhere
+  // and properly handle errors.
+  main().catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+  });
