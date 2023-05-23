9ac3166045674d3f3484dd357b739a4b6d59bf8f
Vincent <v3rdugo@yahoo.com>
2023-05-22 23:15:36 -0700

    add basic properties of ERC-20 standard

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -5,7 +5,9 @@ import "hardhat/console.sol";
 
 contract Token
 {
-    string public name = "My Token";
-
+    string public name = "Vince Coin";
+    string public symbol = 'VIN';
+    uint256 public decimals = 18;
+    uint256 public totalSupply = 1000000 * (10**decimals); // 1000000 * 10^18
 
 }
