0b444af6d9f89c50b434d2ed712deddfa3b31947
Vincent <v3rdugo@yahoo.com>
2023-05-22 23:45:46 -0700

    pass constructor arguments

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -5,9 +5,16 @@ import "hardhat/console.sol";
 
 contract Token
 {
-    string public name = "Vince Coin";
-    string public symbol = 'VIN';
+    string public name;
+    string public symbol;
     uint256 public decimals = 18;
-    uint256 public totalSupply = 1000000 * (10**decimals); // 1000000 * 10^18
+    uint256 public totalSupply;
+
+    constructor(string memory _name, string memory _symbol, uint256 _totalSupply)
+    {
+        name = _name;
+        symbol = _symbol;
+        totalSupply = _totalSupply * (10**decimals); // 1000000 * 10^18
+    }
 
 }
